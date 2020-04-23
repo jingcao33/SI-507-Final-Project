f6baf6fb74a65e435d499db51c596d71ce2c7945
jingcao33 <jingcao@umich.edu>
2020-04-23 19:45:04 -0400

    add complaint info

--- text.py
+++ text.py
@@ -1,14 +1,17 @@
 import requests
 import json
 import secret
-# from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as bs
 import csv
 import sqlite3
 
 API_KEY = secret.API_KEY
+DB_NAME = 'sf_restaurants.sqlite'
 
-
+API_CACHE = {}
+HTML_CACHE = {}
 API_CACHE_FILE = 'api_cache.json'
+HTML_CACHE_FILE = 'html_api.json'
 
 
 # oauth = OAuth1(API_KEY,
@@ -24,25 +27,28 @@ headers = {
 
 
 
-# # cache html
-# def scrape_html(template_url):
-#    # session = dryscrape.Session()
-#    # session.visit(template_url)
-#    # response = session.body()
-#    response = requests.get(template_url)
-#    text = response.text
-#    soup = bs(text, 'html.parser')
-#    template_list = soup.find('html')
-#    return template_list
+# cache html
+dph_url = 'https://www.sfdph.org/dph/EH/Food/default.asp'
+def scrape_html(url=dph_url):
+   response = requests.get(url)
+   text = response.text
+   return text
 
 
-# def scrape_html_with_cache(template_url):
-#    if template_url in HTML_CACHE:
-#       return HTML_CACHE[template_url]
-#    else:
-#       HTML_CACHE[template_url] = scrape_html(template_url)
-#       save_cache(HTML_CACHE, HTML_CACHE_FILE)
-#       return HTML_CACHE[template_url]
+def scrape_html_with_cache(url=dph_url):
+   if url in HTML_CACHE:
+      return HTML_CACHE[url]
+   else:
+      HTML_CACHE[url] = scrape_html(url)
+      save_cache(HTML_CACHE, HTML_CACHE_FILE)
+      return HTML_CACHE[url]
+
+def get_contact_info(text):
+   soup = bs(text, 'html.parser')
+   main_content = soup.find('div', id='pg_content_main_full')
+   all_p = main_content.find_all('p', recursive=False)
+   call_info = all_p[-5:-2]
+   return call_info
 
 
 def construct_unique_key(baseurl, params):
@@ -57,13 +63,12 @@ def construct_unique_key(baseurl, params):
 
 # cache api
 def request_api(baseurl, params):
-   response = requests.get(url=baseurl, params=params, headers=headers, )
+   response = requests.get(url=baseurl, params=params, headers=headers)
    # print(response)
    body = response.text
    result = json.loads(body)
    return result
 
-API_CACHE = {}
 
 def request_api_with_cache(baseurl, params):
    request_key = construct_unique_key(baseurl, params)
@@ -71,7 +76,7 @@ def request_api_with_cache(baseurl, params):
       return API_CACHE[request_key]
    else:
       API_CACHE[request_key] = request_api(baseurl, params)
-      save_cache(API_CACHE)
+      save_cache(API_CACHE, API_CACHE_FILE)
       return API_CACHE[request_key]
 
 
@@ -86,21 +91,12 @@ def open_cache(filename):
    return cache_dict
 
 
-def save_cache(cache_dict):
+def save_cache(cache_dict, filename):
    dumped_json_cache = json.dumps(cache_dict)
-   fw = open(API_CACHE_FILE,"w")
+   fw = open(filename,"w")
    fw.write(dumped_json_cache)
    fw.close()
 
-
-# HTML_CACHE = open_cache(HTML_CACHE_FILE)
-
-
-# scrape_html_with_cache(template_url)
-
-
-
-DB_NAME = 'sf_restaurants.sqlite'
 
 def create_db():
    conn = sqlite3.connect(DB_NAME)
@@ -193,9 +189,6 @@ def load_insepction():
    conn.commit()
    conn.close()
 
-create_db()
-load_insepction()
-
 
 def load_business():
    select_inspection_sql = '''
@@ -219,17 +212,25 @@ def load_business():
       }
       try:
          res = request_api_with_cache(baseurl=match_url, params=params)
-         print(business_name)
+         # print(business_name)
          business_id = res['businesses'][0]['id']
          business_ids.append(business_id)
       except:
-         print(business_name)
+         # print(business_name)
          pass
    return business_ids
 
 
 categories = []
 def insert_business(business_ids):
+   is_closed = None
+   name = None
+   phone = None
+   review_count = None
+   rating = None
+   zipcode = None
+   price = None
+
    insert_business_sql = '''
       INSERT INTO Business
       VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)
@@ -240,16 +241,36 @@ def insert_business(business_ids):
    for business_id in business_ids:
       response = request_api_with_cache(baseurl=detail_url+business_id,
                                        params={'locale':'en_US'})
-      if response['is_closed'] == False:
+      # print(response)
+      try:
+         if response['is_closed'] == True:
+            is_closed = 1
+      except:
          is_closed = 0
-      else:
-         is_closed = 1
-      name = response['name']
-      phone = response['display_phone']
-      review_count = response['review_count']
-      rating = response['rating']
-      zipcode = response['location']['zip_code']
-      price = response['price']
+      try:
+         name = response['name']
+      except:
+         pass
+      try:
+         phone = response['display_phone']
+      except:
+         phone = None
+      try:
+         review_count = response['review_count']
+      except:
+         phone = None
+      try:
+         rating = response['rating']
+      except:
+         rating = None
+      try:
+         zipcode = response['location']['zip_code']
+      except:
+         pass
+      try:
+         price = response['price']
+      except:
+         price = None
 
       cur.execute(insert_business_sql, [
          name,
@@ -261,12 +282,15 @@ def insert_business(business_ids):
          price
       ])
 
-      for category in response['categories']:
-         if category['title'] in categories:
-            pass
-         else:
-            categories.append(category['title'])
-
+      try:
+         for category in response['categories']:
+            if category['title'] in categories:
+               pass
+            else:
+               categories.append(category['title'])
+      except:
+         pass
+   # print(categories)
    conn.commit()
    conn.close()
 
@@ -280,43 +304,68 @@ def insert_cat():
    cur = conn.cursor()
    for category in categories:
       cur.execute(insert_cat_sql, [category])
+   conn.commit()
+   conn.close()
+
+
+def load_categories(business_ids):
+   select_cat_id_sql = '''
+      SELECT id
+      FROM Cat
+      WHERE name = ?
+   '''
+
+   select_business_id_sql = '''
+      SELECT id
+      FROM Business
+      WHERE name = ?
+   '''
+
+   insert_categories_sql = '''
+      INSERT INTO Categories
+      VALUES (NULL, ?, ?)
+   '''
+
    conn = sqlite3.connect(DB_NAME)
    cur = conn.cursor()
 
+   for business_id in business_ids:
+      response = request_api_with_cache(baseurl=detail_url+business_id,
+                                        params={'locale': 'en_US'})
+      # print(response)
+      cat_id = None
+      bus_id = None
+
+      try:
+         for category in response['categories']:
+            cur.execute(select_cat_id_sql, [category['title']])
+            cat_id = cur.fetchone()[0]
+            # print(cat_id)
+            # print(response['name'])
+            # print('----')
+            cur.execute(select_business_id_sql, [response['name']])
+            bus_id = cur.fetchone()[0]
+            print(bus_id)
+            cur.execute(insert_categories_sql, [
+               bus_id,
+               cat_id
+            ])
+      except:
+         # print(business_id)
+         pass
+
+   conn.commit()
+   conn.close()
 
 
-
-   #          cur.execute(select_cat_id_sql, [category])
-   #          res = cur.fetchone()
-   #          categories[category] = res[0]
-
-
-
-   # select_cat_id_sql = '''
-   #    SELECT id
-   #    FROM Cat
-   #    WHERE name = ?
-   # '''
-
-   # select_business_id_sql = '''
-   #    SELECT id
-   #    FROM Business
-   #    WHERE name = ?
-   # '''
-
-   # insert_categories_sql = '''
-   #    INSERT INTO Categories
-   #    VALUES (NULL, ?, ?)
-   # '''
-
-
-
-   
-   # b_id = 
-   # response = requests.get(detail_url+b_id)
-
-
-API_CACHE = open_cache(API_CACHE_FILE)
-ids = load_business()
-insert_business(ids)
-insert_cat()
+# create_db()
+# load_insepction()
+HTML_CACHE = open_cache(HTML_CACHE_FILE)
+call_info = get_contact_info(scrape_html_with_cache())
+# print(call_info)
+# API_CACHE = open_cache(API_CACHE_FILE)
+# ids = load_business()
+# # print(len(ids))
+# insert_business(ids)
+# insert_cat()
+# load_categories(ids)
